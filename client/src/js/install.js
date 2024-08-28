e988bbe19172e9a46f3223ed8d9d92b91dca8938
Elena Papanikolas <epapanikolas@hotmail.com>
2024-08-28 14:45:15 -0600

    Added logic for installing the PWA in install.js and updated service worker in webpack.config.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,36 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    // Prevents default
+    event.preventDefault();
+    // Stores the triggered event
+    window.deferredPrompt = event;
+    // Shows the install button
+    butInstall.style.display = 'block';
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+
+    const promptEvent = window.deferredPrompt;
+
+    if (!promptEvent) {
+        return;
+    }
+
+    // Show prompt
+    promptEvent.prompt();
+    // Resets the deferred prompt variable, it can only be used once.
+    window.deferredPrompt = null;
+    // Hides install button
+    butInstall.style.display = 'none';
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    // Clears prompt
+    window.deferredPrompt = null;
+    // PWA was installed
+    console.log('JATE installed successfully!');
+});
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const { GenerateSW, InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -22,6 +22,11 @@ module.exports = () => {
        new HtmlWebpackPlugin({
         template: './index.html',
         title: 'JATE'
+      }),
+      // Creates service worker
+      new GenerateSW({
+        clientsClaim: true,
+        skipWaiting: true,
       }),
       // Injects our custom service worker
       new InjectManifest({
